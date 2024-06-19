17aefa7e4ea24fde7fedd68a8d8dac665694c253
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-06-20 00:14:09 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mohsin Ali Ayub on 20.06.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
